dacb65733ad5aff56cbdcc779a0f1e3ab56bad87
Edilbert Loquine <e.loquine00@gmail.com>
2019-06-05 07:55:04 -0700

    WIP

--- src/main/java/com/loquinecrud/loquinecrud/controller/BookController.java
+++ src/main/java/com/loquinecrud/loquinecrud/controller/BookController.java
@@ -1,57 +1,72 @@
 package com.loquinecrud.loquinecrud.controller;
 
+import com.loquinecrud.loquinecrud.exception.ResourceNotFoundException;
 import com.loquinecrud.loquinecrud.model.Author;
 import com.loquinecrud.loquinecrud.model.Book;
+import com.loquinecrud.loquinecrud.service.AuthorRepository;
 import com.loquinecrud.loquinecrud.service.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
 
-@RequestMapping("/api/v1/books")
+@RequestMapping("/api/v1")
 @RestController
 public class BookController {
 
     @Autowired
+    private AuthorRepository authorRepository;
+
+    @Autowired
     private BookRepository bookRepository;
 
-    @GetMapping
-    public List<Book> index() {
-        return bookRepository.findAll();
+    @GetMapping("/authors/{authorId}/books")
+    public List<Book> index(@PathVariable long authorId) {
+        return bookRepository.findByAuthorId(authorId);
     }
 
-    @PostMapping
-    public Book create(@Valid @RequestBody Book book) {
-        return bookRepository.save(book);
+    @PostMapping("/authors/{authorId}/books")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Book create(@PathVariable long authorId, @Valid @RequestBody Book book) {
+        return authorRepository.findById(authorId).map(author -> {
+                book.setAuthor(author);
+
+                return bookRepository.save(book);
+            }).orElseThrow(() -> new ResourceNotFoundException("Author with id " + authorId + "not found."));
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Book> get(@PathVariable long id) {
+    @GetMapping("/authors/{authorId}/books/{id}")
+    public ResponseEntity<Book> get(@PathVariable long authorId, @PathVariable long id) {
+        if (!authorRepository.existsById(authorId)) {
+            throw new ResourceNotFoundException("Author with id " + authorId + "not found.");
+        }
+
         return bookRepository.findById(id).map(book -> ResponseEntity.ok().body(book))
-                .orElse(ResponseEntity.notFound().build());
+                .orElseThrow(() -> new ResourceNotFoundException("Book with id " + id + "not found."));
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<Book> update(@PathVariable long id, @Valid @RequestBody Book book) {
-        return bookRepository.findById(id).map(data -> {
-            data.setName(book.getName());
-            data.setDescription(book.getDescription());
-            data.setAuthor(book.getAuthor());
-            data.setPublished_at(book.getPublished_at());
-
-            Book updated = bookRepository.save(data);
-
-            return ResponseEntity.ok().body(updated);
-        }).orElse(ResponseEntity.notFound().build());
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<?> delete(@PathVariable long id) {
-        return bookRepository.findById(id).map(book -> {
-            bookRepository.deleteById(id);
-            return ResponseEntity.ok().build();
-        }).orElse(ResponseEntity.notFound().build());
-    }
+//    @PutMapping("/{id}")
+//    public ResponseEntity<Book> update(@PathVariable long id, @Valid @RequestBody Book book) {
+//        return bookRepository.findById(id).map(data -> {
+//            data.setName(book.getName());
+//            data.setDescription(book.getDescription());
+//            data.setAuthor(book.getAuthor());
+//            data.setPublished_at(book.getPublished_at());
+//
+//            Book updated = bookRepository.save(data);
+//
+//            return ResponseEntity.ok().body(updated);
+//        }).orElse(ResponseEntity.notFound().build());
+//    }
+//
+//    @DeleteMapping("/{id}")
+//    public ResponseEntity<?> delete(@PathVariable long id) {
+//        return bookRepository.findById(id).map(book -> {
+//            bookRepository.deleteById(id);
+//            return ResponseEntity.ok().build();
+//        }).orElse(ResponseEntity.notFound().build());
+//    }
 }
--- src/main/java/com/loquinecrud/loquinecrud/service/BookRepository.java
+++ src/main/java/com/loquinecrud/loquinecrud/service/BookRepository.java
@@ -4,8 +4,8 @@ import com.loquinecrud.loquinecrud.model.Author;
 import com.loquinecrud.loquinecrud.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
+import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
-//    Optional<Author> findByAuthorId(Long id, Long author_id);
+    List<Book> findByAuthorId(Long id);
 }
